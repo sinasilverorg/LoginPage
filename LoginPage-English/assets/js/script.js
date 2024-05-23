11d931319788f1e4465d1e6edc664f67b80fe13d
Sina <sinasilverofficial@gmail.com>
2024-05-24 00:28:24 +0330

    installation repository and coding Html and css and js files

---
+++ LoginPage-English/assets/js/script.js
@@ -0,0 +1,16 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+  container.classList.add('active');
+});
+
+loginBtn.addEventListener('click', () => {
+  container.classList.remove('active');
+});
+
+
+    // Developed by Hero Expert
+    // Telegram channel: @HeroExpert_ir
+    // Telegram channel: @NetworkExpert_com
